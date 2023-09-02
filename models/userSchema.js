82925750350d74d028661f84f25a3f55f006c203
meet112114 <118681492+meet112114@users.noreply.github.com>
2023-09-02 22:19:24 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,37 +1,32 @@
 const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema({
-    
-    title:{
-        type: String,
-        required: true,
-    },
-    category:{
-        type: String,
-        required: true,
-    },
-    description:{
-        type: String,
-        required: true,
-    },
-    rating:{
-        type: Number,
-        required: true,       
-    },
-    image:{
-        type: String,
-        required: true,
-    },
-    location:{
-        type: String,
-        required: true,
-    },
-    images: {
-        type: String,
-        required: true,
-    }
-
+  title: {
+    type: String,
+    required: true,
+  },
+  category: [{ type: String, required: true }],
+  description: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: String,
+    required: true,
+  },
 });
 
-const Place = mongoose.model('USER',placeSchema);
+const Place = mongoose.model("USER", placeSchema);
 module.exports = Place;
